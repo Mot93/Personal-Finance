316e1d4796f14176eb293b91301afb48e1882777
Mattia <mattia.rubini@gmail.com>
2019-04-16 12:53:00 +0200

    Introducing the Savings, added a new field to expences

--- finance_test/financedb_test.go
+++ finance_test/financedb_test.go
@@ -10,17 +10,17 @@ import (
 // >sqlite3 testfinance.db
 // >.exit
 
-// TestExpence test all function working with the Expence struct
-func TestExpence(t *testing.T) {
-	var e1, e2 financedatabase.Expence
-	e1.Constructor("netflix", 15.0, "2017-06-22", "", "work")
-	e2.Constructor("netflix", 15.0, "2017-06-22", "", "work")
-	if !e1.Equals(e2) {
-		t.Errorf("Equals of Expence doesn't work should be equals:\n   %v\n   %v", e1, e2)
+// TestAmount test all function working with the Amount struct
+func TestAmount(t *testing.T) {
+	var a1, a2 financedatabase.Amount
+	a1.Constructor("netflix", 15.0, "2017-06-22", "", "work")
+	a2.Constructor("netflix", 15.0, "2017-06-22", "", "work")
+	if !a1.Equals(a2) {
+		t.Errorf("Equals of Expence doesn't work should be equals:\n   %v\n   %v", a1, a2)
 	}
-	e2.Constructor("Rent", 400.0, "2006-05-15", "", "utilities")
-	if e1.Equals(e2) {
-		t.Errorf("Equals of Expence doesn't work should be differen:\n   %v\n   %v", e1, e2)
+	a2.Constructor("Rent", 400.0, "2006-05-15", "", "utilities")
+	if a1.Equals(a2) {
+		t.Errorf("Equals of Expence doesn't work should be differen:\n   %v\n   %v", a1, a2)
 	}
 }
 
@@ -56,26 +56,40 @@ func closeDB(t *testing.T, dbName string) {
 func DBCategory(t *testing.T) {
 	financedatabase.StoreCategory("work")
 	financedatabase.StoreCategory("utilities")
+	categories := financedatabase.ReadAllCategories()
+	if categories[0] != "utilities" || categories[1] != "work" {
+		t.Errorf("List of utlities incorrect:\n   utilities %v\n   work %v", categories[0], categories[1])
+	}
 }
 
 // DBExpences tests all fuction working with the Expense struct and the database
 // Launched by TestDatabase
 func DBExpences(t *testing.T) {
-	var e []financedatabase.Expence
-	var e1, e2 financedatabase.Expence
+	var e []financedatabase.Amount
+	var e1, e2 financedatabase.Amount
+	// Checking that the expences are stored and retrived corectly
 	e1.Constructor("netflix", 15.0, "2017-06-22", "", "work")
 	financedatabase.StoreExpence(e1)
 	e2 = financedatabase.ReadExpenceByid(1)
 	if !e1.Equals(e2) {
 		t.Errorf("Expence wasn't stored corectly:\n   %v\n   %v", e1, e2)
 	}
+	// Checking if all the expences are retrived corectly
 	e2.Constructor("Rent", 400.0, "2006-05-15", "", "utilities")
 	financedatabase.StoreExpence(e2)
-	e = financedatabase.ReadAllExpence()
+	e = financedatabase.ReadAllExpences()
 	if !e1.Equals(e[1]) || !e2.Equals(e[0]) {
 		t.Errorf("All Expences weren't returned in a proper way:\n   e1   = %v\n   e[1] = %v\n   e2   = %v\n   e[0] = %v", e1, e[1], e2, e[0])
 	}
 	if e1.Equals(e[0]) || e2.Equals(e[1]) {
 		t.Errorf("All Expences weren't returned in a proper way:\n   e1   = %v\n   e[1] = %v\n   e2   = %v\n   e[0] = %v", e1, e[1], e2, e[0])
+	}
+	// Checking that expences are updated corectly
+	e1 = financedatabase.ReadExpenceByid(1)
+	e1.Constructor("new expence", 66, "1993-06-02", "", "work")
+	financedatabase.UpdateExpence(e1)
+	e2 = financedatabase.ReadExpenceByid(1)
+	if e1 != e2 {
+		t.Errorf("Expences updated failed:\n   %v\n   %v", e1, e2)
 	}
 }
--- financedatabase/financedb.go
+++ financedatabase/financedb.go
@@ -9,30 +9,33 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Expence is a struct containing the data of an expence in similar fashion of the database
-type Expence struct {
-	id       int
-	name     string
-	sum      float32
-	start    string
-	end      string
-	category string
+// Amount contains the data of both savings and expences
+// The two have identical structur but serve two differen purpose
+type Amount struct {
+	id         int
+	name       string
+	sum        float32
+	start      string
+	end        string
+	category   string
+	recurrency int
 }
 
-// Constructor fills the struct excpet for the id field
+// Constructor fills the struct Amount except for the id field
 // The id field is managed by the database
-func (e *Expence) Constructor(name string, sum float32, start string, end string, category string) {
-	e.name = name
-	e.sum = sum
-	e.start = start
-	e.end = end
-	e.category = category
+func (a *Amount) Constructor(name string, sum float32, start string, end string, category string) {
+	a.name = name
+	a.sum = sum
+	a.start = start
+	a.end = end
+	a.category = category
+	a.recurrency = 0
 }
 
 // Equals will compare that each field of Expence are equals
 // Except for the field ID
-func (e Expence) Equals(e2 Expence) bool {
-	if strings.Compare(e.name, e2.name) == 0 && e.sum == e2.sum && strings.Compare(e.start, e2.start) == 0 && strings.Compare(e.end, e2.end) == 0 {
+func (a Amount) Equals(a2 Amount) bool {
+	if strings.Compare(a.name, a2.name) == 0 && a.sum == a2.sum && strings.Compare(a.start, a2.start) == 0 && strings.Compare(a.end, a2.end) == 0 {
 		return true
 	}
 	return false
@@ -51,15 +54,16 @@ func InitDB(name string) {
 	if strings.Compare(name, "") == 0 {
 		name = "finance.db"
 	}
-	db = connectdatabase(name)
-	createtablecategory()
-	createtableexpences()
+	db = connectDatabase(name)
+	createTableCategory()
+	createTableExpences()
+	createTableSavings()
 }
 
 // startdatabase initiate a connection with the database that already exist
 // if the database doesn't exist, it creates a new one
 // the path to the database is determined by filepath
-func connectdatabase(filepath string) (db *sql.DB) {
+func connectDatabase(filepath string) (db *sql.DB) {
 	db, err := sql.Open("sqlite3", filepath)
 	if err != nil {
 		panic(fmt.Errorf("Erorr connecting to the database: %v", err))
@@ -70,11 +74,11 @@ func connectdatabase(filepath string) (db *sql.DB) {
 	return db
 }
 
-// createtable creates table, provided the righ string
-func createtable(table string, tableName string) {
-	_, err := db.Exec(table)
+// executeCommand execute an sql command
+func executeCommand(command string, operation string) {
+	_, err := db.Exec(command)
 	if err != nil {
-		panic(fmt.Errorf("Erorr creating table %v: %v", tableName, err))
+		panic(fmt.Errorf("Erorr %v: %v", operation, err))
 	}
 }
 
@@ -91,14 +95,53 @@ func storeItem(item string, itemName string) {
 	}
 }
 
+//
+func retriveSingleAmount(sqlQuery string, readingElement string) Amount {
+	// return the result of a select
+	row, err := db.Query(sqlQuery)
+	if err != nil {
+		panic(fmt.Errorf("Error reading %v by id: %v", readingElement, err))
+	}
+	defer row.Close()
+	// reading all element in the row
+	var am Amount
+	row.Next()
+	err2 := row.Scan(&am.id, &am.name, &am.sum, &am.start, &am.end, &am.category, &am.recurrency)
+	if err2 != nil {
+		panic(fmt.Errorf("Error reading saving by id: %v", err2))
+	}
+	return am
+}
+
+//
+func retriveAllAmounts(sqlQuery string, readingElement string) []Amount {
+	// return the result of a select
+	row, err := db.Query(sqlQuery)
+	if err != nil {
+		panic(fmt.Errorf("Error reading all %v: %v", readingElement, err))
+	}
+	defer row.Close()
+	// reading all element in the row
+	var am []Amount
+	for row.Next() {
+		var a Amount
+		err2 := row.Scan(&a.id, &a.name, &a.sum, &a.start, &a.end, &a.category, &a.recurrency)
+		if err2 != nil {
+			panic(fmt.Errorf("Error reading all expence: %v", err2))
+		}
+		am = append(am, a)
+	}
+	return am
+}
+
 // createtablecategory creates the table category if it doensn't already exist
-func createtablecategory() {
+func createTableCategory() {
 	categoryTable := `
 	CREATE TABLE IF NOT EXISTS categories(
 		category TEXT PRIMARY KEY
 	);
 	`
-	createtable(categoryTable, "categories")
+	executeCommand(categoryTable, "create table categories")
 }
 
 // StoreCategory add a category to the table categories
@@ -111,8 +154,37 @@ func StoreCategory(category string) {
 	storeItem(sqlCategory, "category")
 }
 
-// createtableexpences creates the table exences if it doensn't already exist
-func createtableexpences() {
+// TODO: add delete categories
+
+// ReadAllCategories return the complete list of categories
+func ReadAllCategories() []string {
+	sqlCategories := `
+	SELECT * FROM categories
+	ORDER BY category
+	`
+	// return the result of a select
+	row, err := db.Query(sqlCategories)
+	if err != nil {
+		panic(fmt.Errorf("Error reading all categories: %v", err))
+	}
+	defer row.Close()
+	// reading all element in the row
+	var categories []string
+	for row.Next() {
+		var category string
+		err2 := row.Scan(&category)
+		if err2 != nil {
+			panic(fmt.Errorf("Error reading all categories: %v", err2))
+		}
+		categories = append(categories, category)
+	}
+	return categories
+}
+
+// TODO: delete expence
+
+// createTableExpences creates the table exences if it doensn't already exist
+func createTableExpences() {
 	// Creting a string with the grave accent because the string contains \n
 	// YYYY-MM-DD
 	// Start at 1
@@ -123,14 +195,15 @@ func createtableexpences() {
 		sum REAL NOT NULL,
 		start TEXT NOT NULL,
 		end TEXT DAFAULT NULL,
-		category TEXT REFERENCES categories(category)
+		category TEXT REFERENCES categories(category),
+		recurrency INTEGER DEFAULT 0
 	);
 	`
-	createtable(expencesTable, "expences")
+	executeCommand(expencesTable, "create table expences")
 }
 
 // StoreExpence adds rows to the expences tables
-func StoreExpence(ex Expence) {
+func StoreExpence(am Amount) {
 	sqlExpence := fmt.Sprintf(`
 	INSERT INTO expences(
 		name,
@@ -139,57 +212,75 @@ func StoreExpence(ex Expence) {
 		end,
 		category
 	) VALUES ('%v', %v, '%v', '%v', '%v');
-	`, ex.name, ex.sum, ex.start, ex.end, ex.category)
+	`, am.name, am.sum, am.start, am.end, am.category)
 	storeItem(sqlExpence, "expence")
 }
 
 // ReadExpenceByid given an id it returns all its values
-func ReadExpenceByid(id int) Expence {
+func ReadExpenceByid(id int) Amount {
 	sqlExpence := fmt.Sprintf(`
 	SELECT * FROM expences
 	WHERE id = %v
 	AND date(start)
 	`, id)
-	// return the result of a select
-	row, err := db.Query(sqlExpence)
-	if err != nil {
-		panic(fmt.Errorf("Error reading expences by id: %v", err))
-	}
-	defer row.Close()
-	// reading all element in the row
-	var ex Expence
-	row.Next()
-	err2 := row.Scan(&ex.id, &ex.name, &ex.sum, &ex.start, &ex.end, &ex.category)
-	if err2 != nil {
-		panic(fmt.Errorf("Error reading expence by id: %v", err2))
-	}
-	return ex
+	return retriveSingleAmount(sqlExpence, "expence")
 }
 
-// ReadAllExpence Return all the expences contained in the db
-// Ordered by start date
-func ReadAllExpence() []Expence {
-	sqlExpence := `
+// ReadAllExpences return all the expences contained in the db
+// Ordered by start from the eldest to the most recent
+func ReadAllExpences() []Amount {
+	sqlExpences := `
 	SELECT * FROM expences
 	ORDER BY date(start)
 	`
-	// return the result of a select
-	row, err := db.Query(sqlExpence)
-	if err != nil {
-		panic(fmt.Errorf("Error reading all expences: %v", err))
-	}
-	defer row.Close()
-	// reading all element in the row
-	var ex []Expence
-	for row.Next() {
-		var e Expence
-		err2 := row.Scan(&e.id, &e.name, &e.sum, &e.start, &e.end, &e.category)
-		if err2 != nil {
-			panic(fmt.Errorf("Error reading all expence: %v", err2))
-		}
-		ex = append(ex, e)
-	}
-	return ex
+	return retriveAllAmounts(sqlExpences, "expences")
 }
 
-// TODO: modifica expences già esistenti
+// UpdateExpence allows to update and expence
+// The only way to set the id of an expence is to get an expence from the database
+// This is intended to make sure that we are operating on the expences specified by the user
+func UpdateExpence(a Amount) {
+	sqlExpence := fmt.Sprintf(`
+	UPDATE expences 
+	SET name = '%v', sum = %v, start = '%v', end = '%v', category = '%v'
+	WHERE id = %v;
+	`, a.name, a.sum, a.start, a.end, a.category, a.id)
+	executeCommand(sqlExpence, "updating expences")
+}
+
+// TODO: add savings & recurent savings
+
+// createTableSavings creates the table savings if it doensn't already exist
+func createTableSavings() {
+	savingsTable := `
+	CREATE TABLE IF NOT EXISTS savings(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL,
+		sum REAL NOT NULL,
+		start TEXT NOT NULL,
+		end TEXT DAFAULT NULL,
+		category TEXT REFERENCES categories(category),
+		recurrency INTEGER DEFAULT 0
+	);
+	`
+	executeCommand(savingsTable, "create table savings")
+}
+
+// ReadSavingByid return the saving rfered by id
+func ReadSavingByid(id int) Amount {
+	sqlSaving := fmt.Sprintf(`
+	SELECT * FROM savings
+	WHERE id = %v
+	`, id)
+	return retriveSingleAmount(sqlSaving, "saving")
+}
+
+// ReadAllSavings return all the expences contained in the db
+// Ordered by start from the eldest to the most recent
+func ReadAllSavings() []Amount {
+	sqlSavings := `
+	SELECT * FROM savings
+	ORDER BY date(start)
+	`
+	return retriveAllAmounts(sqlSavings, "savings")
+}
